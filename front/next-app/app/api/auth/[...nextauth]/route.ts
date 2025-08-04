7d1e82e32109051d072ad64f5452c46605dc0328
shion <shion48691811@gmail.com>
2025-08-05 04:38:17 +0900

    google連携の途中まで完成

---
+++ front/next-app/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,14 @@
+// app/api/auth/[...nextauth]/route.ts
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.AUTH_GOOGLE_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+    }),
+  ],
+})
+
+export { handler as GET, handler as POST }
---
+++ front/next-app/app/login/Auth.tsx
@@ -0,0 +1,11 @@
+// app/login/Auth.tsx
+"use client";
+
+
+import { auth } from "@/auth";
+
+export default async function LoginPage() {
+  const session = await auth(); 
+
+  return <p>{session ? "ログイン中" : "未ログイン"}</p>;
+}
