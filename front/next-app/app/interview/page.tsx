b53c40fef3636bde729e3e1d0279763b7fe10984
shion <shion48691811@gmail.com>
2025-08-08 07:56:53 +0900

    インタビューコンポーネントを実装

---
+++ front/next-app/app/interview/page.tsx
@@ -0,0 +1,9 @@
+import Interview from "../components/interview";
+
+export default function InterviewPage() {
+    return (
+        <div>
+            <Interview title="インタビュー" date="2025-01-01" content="インタビュー内容インタビュー内容インタビュー内容インタビュー内容インタビュー内容インタビュー内容" />
+        </div>
+    )
+}
