81cce69c3587ef9e1d89d3c8654725f8cd925739
tachi <shun.050614@icloud.com>
2025-08-09 01:58:31 +0900

    マップ埋め込み用のcomponentsとか

---
+++ front/next-app/app/components/map-content.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { APIProvider, Map } from '@vis.gl/react-google-maps';
+
+export const MapContent = () => {
+    return (
+        <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ''}>
+            <div style={{ width: '100%', height: '80vh' }}>
+                <Map
+                    defaultZoom={15}
+                    defaultCenter={{ lat: 35.170915, lng: 136.881537 }}
+                    style={{ width: '100%', height: '100%' }}
+                />
+            </div>
+        </APIProvider>
+    );
+};
---
+++ front/next-app/app/map/page.tsx
@@ -0,0 +1,9 @@
+import { MapContent } from '@/components/map-content';
+
+export default function Page() {
+    return (
+    <div className="h-svh">
+        <MapContent />
+    </div>
+    );
+}
